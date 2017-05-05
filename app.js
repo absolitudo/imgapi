0625d112db334250047bbab9aed398e21371a477
absolitudo <absolitudo@gmail.com>
2017-05-05 13:26:21 +0200

    more basic stuff

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require("express"),
     app = express(),
     mongoose = require("mongoose"),
-    searchSchema = new Schema({
+    searchSchema = new mongoose.Schema({
         "term": String,
         "when": String
     }),
     SearchQueries = mongoose.model('SearchQueries', searchSchema),
-    apiKey = process.env.API_KEY,
-    dbURI = process.env.MONGODB_URI;
+    apiKey = process.env.API_KEY;
+
+mongoose.connect(process.env.MONGODB_URI);
+new SearchQueries({
+    "term": "test",
+    "when": new Date()
+}).save(function(err) {
+    console.log("data saved");
+});
+app.get("/api/latest/imagesearch", function(req, res) {
+    SearchQueries.find({}, function(err, data) {
+        data.forEach(function(dataPiece) {
+            res.send(dataPiece);
+        });
+    });
+});
+app.listen(process.env.PORT || 8080);
+console.log("server up");
