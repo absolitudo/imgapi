a69d430d90dde7f676f45944a2bd56921935f46c
absolitudo <absolitudo@gmail.com>
2017-05-06 16:34:41 +0200

    basic database functionality

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
+require('dotenv').config();
 var express = require("express"),
     app = express(),
     mongoose = require("mongoose"),
-    searchSchema = new mongoose.Schema({
+    querySchema = new mongoose.Schema({
         "term": String,
         "when": String
     }),
-    SearchQueries = mongoose.model('SearchQueries', searchSchema),
-    apiKey = process.env.API_KEY;
+    SearchQueries = mongoose.model('SearchQueries', querySchema);
 
 mongoose.connect(process.env.MONGODB_URI);
-new SearchQueries({
-    "term": "test",
-    "when": new Date()
-}).save(function(err) {
-    console.log("data saved");
-});
 app.get("/api/latest/imagesearch", function(req, res) {
-    SearchQueries.find({}, function(err, data) {
-        data.forEach(function(dataPiece) {
-            res.send(dataPiece);
-        });
+    SearchQueries.find({}, {
+        "_id": 0,
+        "__v": 0
+    }, function(err, data) {
+        res.send(data.reverse());
     });
+});
+
+app.get("/api/imagesearch/:term", function(req, res) {
+    SearchQueries.findOne({}, function(err, data) {
+        if(err) throw err;
+        data.remove();
+    });
+    SearchQueries({
+        "term": req.params.term,
+        "when": new Date().toISOString()
+    }).save(function(err) {
+        if(err) throw err;
+    });
+
+    // make the actual search and display the results
 });
 app.listen(process.env.PORT || 8080);
 console.log("server up");
